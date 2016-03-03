b13daeb6347e42248787d8faa625f16f46db1e2d
ArthurHlt <arthurh.halet@gmail.com>
2016-03-03 20:19:05 +0100

    make it work for the first time

--- src/common/setup-java-prepackaging.sh
+++ src/common/setup-java-prepackaging.sh
@@ -7,7 +7,7 @@ cd ${BUILD_DIR}
 
 tar zxfv ${BUILD_DIR}/openjdk/${JAVA_TAR_BALL}
 
-export JAVA_HOME=${BUILD_DIR}/${JAVA_VERSION}
+if [ -z ${JAVA_HOME+x} ]; then export JAVA_HOME=${BUILD_DIR}/${JAVA_VERSION}; fi
 echo $JAVA_HOME
 
 cd ${BUILD_DIR}
@@ -17,8 +17,6 @@ export MAVEN_HOME=${BUILD_DIR}/${MAVEN_VERSION}
 export PATH=${MAVEN_HOME}/bin:$PATH
 
 cleanup_java() {
-  rm -rf ${JAVA_HOME}
-  rm -rf ${MAVEN_HOME}
   rm -rf ${BUILD_DIR}/openjdk
   rm -rf ${BUILD_DIR}/maven
   rm -rf ${BUILD_DIR}/target
